ca29e220fcb54d044412c4430718887d67898754
Cameronwood611 <cwood611@uab.edu>
2021-10-25 19:54:46 -0500

    finish ai

--- aiplayer.py
+++ aiplayer.py
@@ -5,23 +5,45 @@ import random
 from player import Player
 
 
-def heuristic(board):
+def heuristic(game, player, opponent):
+    board = game.data
     # 0 is empty
     # 1 is player 1
     # 2 is player 2
-    
-    ...
+
+    WEIGHTS = [
+        [120, -20,  20,   5,   5,  20, -20, 120],
+        [-20, -40,  -5,  -5,  -5,  -5, -40, -20],
+        [20,  -5,  15,   3,   3,  15,  -5,  20],
+        [5,  -5,   3,   3,   3,   3,  -5,   5],
+        [5,  -5,   3,   3,   3,   3,  -5,   5],
+        [20,  -5,  15,   3,   3,  15,  -5,  20],
+        [-20, -40,  -5,  -5,  -5,  -5, -40, -20],
+        [120, -20,  20,   5,   5,  20, -20, 120]
+    ]
+    total = 0
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] == player:
+                total += WEIGHTS[i][j]
+            elif board[i][j] == opponent:
+                total -= WEIGHTS[i][j]
+    return total
+
+
 
 
 def max_value(game, alpha, beta, d) -> tuple:
-    if game.terminal() or d == 0:
+    if game.terminal():
         return (game.utility(game.player()), None)
+    if d == 0:
+        score = heuristic(game, game.player(), game.otherplayer())
+        return (score, None)
 
     v = float("-inf")
     move = None
     for action in game.actions():
         v2, _ = min_value(game.result(action), alpha, beta, d - 1)
-        v2 += heuristic(game)
         if v2 > v:
             v, move = v2, action
             alpha = max(alpha, v)
@@ -31,14 +53,16 @@ def max_value(game, alpha, beta, d) -> tuple:
 
 
 def min_value(game, alpha, beta, d) -> tuple:  # (utility, move)
-    if game.terminal() or d == 0:
+    if game.terminal():
         return (game.utility(game.otherplayer()), None)
-
+    if d == 0:
+        score = heuristic(game, game.otherplayer(), game.player())
+        return (score, None)
+    
     v = float("+inf")
     move = None
     for action in game.actions():
         v2, _ = max_value(game.result(action), alpha, beta, d - 1)
-        v2 += heuristic(game.data)
         if v2 < v:
             v, move = v2, action
             beta = min(beta, v)
@@ -63,7 +87,6 @@ class AIPlayer(Player):
         # To launch a game using this AI, run $ python3 play.py
 
         return alpha_beta(board)
-        # return random.sample(board.actions(),1)[0]
     def player(self):
         return self.playerN
 
